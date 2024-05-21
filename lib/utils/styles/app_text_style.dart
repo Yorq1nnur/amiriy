41f03da548752cb4ae513d1226d37d852575654e
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-21 20:27:37 +0500

    1

--- lib/utils/styles/app_text_style.dart
+++ lib/utils/styles/app_text_style.dart
@@ -1,5 +1,6 @@
 import 'package:amiriy/utils/colors/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTextStyle {
   //Inter
