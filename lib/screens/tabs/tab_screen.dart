71ec54442fe1d31e9f9a06cb51ff9ff56fd4f394
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-05-24 10:22:41 +0500

    1

--- lib/screens/tabs/tab_screen.dart
+++ lib/screens/tabs/tab_screen.dart
@@ -155,13 +155,16 @@
 //     );
 //   }
 // }
-
 import 'dart:math';
+import 'package:amiriy/bloc/auth/auth_bloc.dart';
+import 'package:amiriy/bloc/auth/auth_state.dart';
+import 'package:amiriy/bloc/form_status/form_status.dart';
+import 'package:amiriy/screens/routes.dart';
 import 'package:amiriy/screens/tabs/book/books_screen.dart';
 import 'package:amiriy/screens/tabs/books/books_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:my_utils/my_utils.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'search/search_screen.dart';
 import 'settings/settings_screen.dart';
 
@@ -172,7 +175,8 @@ class TabScreen extends StatefulWidget {
   State<TabScreen> createState() => _TabScreenState();
 }
 
-class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMixin {
+class _TabScreenState extends State<TabScreen>
+    with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
   late AnimationController _controller;
   late Animation<double> _animation;
@@ -230,14 +234,18 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
                   child: Container(
                     width: 8.0,
                     height: 8.0,
-                    decoration:  BoxDecoration(
+                    decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Theme.of(context).primaryColor,
                     ),
                   ),
                 );
               }),
-            Icon(icon, color: Theme.of(context).iconTheme.color, size: Theme.of(context).iconTheme.size,),
+            Icon(
+              icon,
+              color: Theme.of(context).iconTheme.color,
+              size: Theme.of(context).iconTheme.size,
+            ),
           ],
         );
       },
@@ -247,7 +255,21 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (p, c) {
+          if (c.status == FormStatus.unauthenticated) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              RouteNames.loginRoute,
+              (context) => false,
+            );
+          }
+        },
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: screens,
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
@@ -273,4 +295,3 @@ class _TabScreenState extends State<TabScreen> with SingleTickerProviderStateMix
     );
   }
 }
-
